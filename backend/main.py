f5199d207f27b9472e7a5f71ae7a57f062ad8a37
Hakim1195 <hakimmathlouthi95@gmail.com>
2026-05-30 03:30:59 +0200

    Bug main.py corrigé

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
-from backend.core.config import settings
+from core.config import settings
 from api import api_router
 from core.database import Base, engine
 from api.sockets.connection_manager import manager as connection_manager
